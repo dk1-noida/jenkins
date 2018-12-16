d5e954696dbb0858c64fc6cc7ec92b9be60d000c
Dinesh Kumar <dkumar@justclean.com>
2018-12-16 18:30:54 +0300

    testing autoabf

--- first.js
+++ first.js
@@ -1,3 +1,5 @@
 console.log("hi, testing!\nBest of luck.");
 
-console.log("hi, commit is also working");
+console.log("hi, commit is also working\n\n");
+
+console.log("second commit is also working");
